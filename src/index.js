81904495786af0bb1f152a1a5a0a8b4cf91b5dbb
AnaLydia Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-11-16 21:25:00 -0800

    commit lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const list = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const taskText = input.value.trim();
+      if (taskText !== "") {
+          const taskItem = createTaskItem(taskText);
+          list.appendChild(taskItem);
+          input.value = "";
+      }
+  });
+
+  function createTaskItem(taskText) {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "x";
+      deleteButton.addEventListener("click", function () {
+          list.removeChild(taskItem);
+      });
+
+      taskItem.appendChild(deleteButton);
+
+      return taskItem;
+  }
 });
+
